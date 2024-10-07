00dc6709e7bf252c594eefac21bf6b6f1a1b7b9e
Kyle Smart <kyle.smart.6.ctr@us.af.mil>
2024-10-07 14:17:18 -0600

    Pushing up changes to add previous color functionality.

--- src/index.types.ts
+++ src/index.types.ts
@@ -1,44 +1,46 @@
 import type {
   ColorButtonElement,
-  ColorButtonProps
-} from '@components/ColorButton/ColorButton'
+  ColorButtonProps,
+} from "@components/ColorButton/ColorButton";
 import type {
   ColorFormats,
-  ColorInput as MuiColorInputValue
-} from '@ctrl/tinycolor'
-import type { PopoverProps as MuiPopoverProps } from '@mui/material/Popover'
-import type { TextFieldProps as MuiTextFieldProps } from '@mui/material/TextField'
+  ColorInput as MuiColorInputValue,
+} from "@ctrl/tinycolor";
+import type { PopoverProps as MuiPopoverProps } from "@mui/material/Popover";
+import type { TextFieldProps as MuiTextFieldProps } from "@mui/material/TextField";
+import type { ReactElement } from "react";
 
 export type MuiColorInputFormat = Extract<
-  'hex' | 'rgb' | 'hex8' | 'hsl' | 'hsv',
+  "hex" | "rgb" | "hex8" | "hsl" | "hsv",
   ColorFormats
->
+>;
 
-export type { ColorButtonProps as MuiColorButtonProps, MuiColorInputValue }
+export type { ColorButtonProps as MuiColorButtonProps, MuiColorInputValue };
 
-type PopoverProps = Omit<MuiPopoverProps, 'anchorEl' | 'open' | 'children'>
+type PopoverProps = Omit<MuiPopoverProps, "anchorEl" | "open" | "children">;
 
 type TextFieldProps = Omit<
   MuiTextFieldProps,
-  'onChange' | 'select' | 'type' | 'multiline' | 'defaultValue'
->
+  "onChange" | "select" | "type" | "multiline" | "defaultValue"
+>;
 
 export type MuiColorInputColors = {
-  hex: string
-  hsl: string
-  hsv: string
-  rgb: string
-  hex8: string
-}
+  hex: string;
+  hsl: string;
+  hsv: string;
+  rgb: string;
+  hex8: string;
+};
 
 export interface MuiColorInputProps extends TextFieldProps {
-  value: MuiColorInputValue
-  adornmentPosition?: 'start' | 'end'
-  Adornment?: ColorButtonElement
-  fallbackValue?: MuiColorInputValue
-  format?: MuiColorInputFormat
-  disablePopover?: boolean
-  isAlphaHidden?: boolean
-  onChange?: (value: string, colors: MuiColorInputColors) => void
-  PopoverProps?: PopoverProps
+  value: MuiColorInputValue;
+  adornmentPosition?: "start" | "end";
+  Adornment?: ColorButtonElement;
+  fallbackValue?: MuiColorInputValue;
+  format?: MuiColorInputFormat;
+  disablePopover?: boolean;
+  isAlphaHidden?: boolean;
+  onChange?: (value: string, colors: MuiColorInputColors) => void;
+  PopoverProps?: PopoverProps;
+  previousColors?: ReactElement[];
 }
