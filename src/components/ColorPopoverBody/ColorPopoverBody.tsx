f203a0082479cb871a70c0aeb9bb20a976f131a7
Kyle Smart <kyle.smart.6.ctr@us.af.mil>
2024-10-09 13:33:47 -0600

    Added launch.json file.

--- src/components/ColorPopoverBody/ColorPopoverBody.tsx
+++ src/components/ColorPopoverBody/ColorPopoverBody.tsx
@@ -1,79 +1,82 @@
-import React, { type ReactElement } from 'react'
-import AlphaSlider from '@components/AlphaSlider/AlphaSlider'
-import ColorSpace from '@components/ColorSpace/ColorSpace'
-import HueSlider from '@components/HueSlider/HueSlider'
-import { buildValueFromTinyColor } from '@shared/helpers/format'
-import { clamp, matchIsNumber } from '@shared/helpers/number'
-import { type HSV, type Numberify, TinyColor } from '@ctrl/tinycolor'
-import Box from '@mui/material/Box'
-import type { MuiColorInputFormat, MuiColorInputProps } from '../../index.types'
+import React, { type ReactElement } from "react";
+import AlphaSlider from "@components/AlphaSlider/AlphaSlider";
+import ColorSpace from "@components/ColorSpace/ColorSpace";
+import HueSlider from "@components/HueSlider/HueSlider";
+import { buildValueFromTinyColor } from "@shared/helpers/format";
+import { clamp, matchIsNumber } from "@shared/helpers/number";
+import { type HSV, type Numberify, TinyColor } from "@ctrl/tinycolor";
+import Box from "@mui/material/Box";
+import type {
+  MuiColorInputFormat,
+  MuiColorInputProps,
+} from "../../index.types";
 
 type ColorPopoverBodyProps = {
-  currentColor: TinyColor
-  format: MuiColorInputFormat
-  isAlphaHidden: MuiColorInputProps['isAlphaHidden']
-  onChange: (value: string) => void
-  previousColors?: ReactElement[]
-}
+  currentColor: TinyColor;
+  format: MuiColorInputFormat;
+  isAlphaHidden: MuiColorInputProps["isAlphaHidden"];
+  onChange: (value: string) => void;
+  previousColors?: ReactElement[];
+};
 
-const startingArray: ReactElement[] = []
+const startingArray: ReactElement[] = [];
 
 const ColorPopoverBody = ({
   currentColor,
   format,
   onChange,
   isAlphaHidden,
-  previousColors = startingArray
+  previousColors = startingArray,
 }: ColorPopoverBodyProps) => {
   const [currentHsv, setCurrentHsv] = React.useState<Numberify<HSV>>(
     currentColor.toHsv()
-  )
+  );
 
   const handleChangeHue = (event: Event, hue: number | number[]) => {
     if (!matchIsNumber(hue)) {
-      return
+      return;
     }
 
-    const newHue = clamp((360 * hue) / 100, 0, 359)
+    const newHue = clamp((360 * hue) / 100, 0, 359);
     setCurrentHsv((prevState) => {
       return {
         ...prevState,
-        h: newHue
-      }
-    })
+        h: newHue,
+      };
+    });
     const tinyColor = new TinyColor({
       ...currentHsv,
       a: currentColor.toHsv().a,
-      h: newHue
-    })
-    onChange?.(buildValueFromTinyColor(tinyColor, format))
-  }
+      h: newHue,
+    });
+    onChange?.(buildValueFromTinyColor(tinyColor, format));
+  };
 
   const handleChangeAlpha = (event: Event, alphaValue: number | number[]) => {
     if (!matchIsNumber(alphaValue)) {
-      return
+      return;
     }
 
-    const tinyColor = currentColor.clone().setAlpha(alphaValue)
-    onChange?.(buildValueFromTinyColor(tinyColor, format))
-  }
+    const tinyColor = currentColor.clone().setAlpha(alphaValue);
+    onChange?.(buildValueFromTinyColor(tinyColor, format));
+  };
 
-  const handleChangeSpace = ({ s, v }: Pick<Numberify<HSV>, 's' | 'v'>) => {
+  const handleChangeSpace = ({ s, v }: Pick<Numberify<HSV>, "s" | "v">) => {
     const tinyColor = new TinyColor({
       h: currentHsv.h,
       a: currentColor.toHsv().a,
       s,
-      v
-    })
+      v,
+    });
     setCurrentHsv((prevState) => {
       return {
         ...prevState,
         s,
-        v
-      }
-    })
-    onChange?.(buildValueFromTinyColor(tinyColor, format))
-  }
+        v,
+      };
+    });
+    onChange?.(buildValueFromTinyColor(tinyColor, format));
+  };
 
   return (
     <Box className="MuiColorInput-PopoverBody">
@@ -111,7 +114,7 @@ const ColorPopoverBody = ({
         </Box>
       ) : null}
     </Box>
-  )
-}
+  );
+};
 
-export default ColorPopoverBody
+export default ColorPopoverBody;
